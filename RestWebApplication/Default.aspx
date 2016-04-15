42f8419bf82a01f8b20aea3fa1a9e84cf8abb5fd
pgaines937 <pgaines937@gmail.com>
2016-04-14 23:46:28 -0700

    Initial commit

---
+++ RestWebApplication/Default.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="RestWebApplication._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <div class="jumbotron">
+        <h1>Welcome to Number Guessing Game</h1>
+        <asp:Label ID="LabelLowerLimit" runat="server" Text="Lower Limit"></asp:Label>
+        <asp:TextBox ID="TextBoxLower" runat="server"></asp:TextBox>
+        <asp:Label ID="LabelUpperLimit" runat="server" Text="Upper Limit"></asp:Label>
+        <asp:TextBox ID="TextBoxUpper" runat="server"></asp:TextBox>
+        <asp:Button ID="ButtonSecretNumber" runat="server" Text="Generate a Secret Number" OnClick="ButtonSecretNumber_Click" />
+        <asp:Label ID="LabelSecret" runat="server" Text="Label" Visible="False"></asp:Label>
+        <br />
+
+        <asp:Label ID="LabelMakeGuess" runat="server" Text="Make a Guess"></asp:Label>
+        <asp:TextBox ID="TextBoxMakeGuess" runat="server"></asp:TextBox>
+        <asp:Button ID="ButtonPlay" runat="server" Text="Play" OnClick="ButtonPlay_Click" />
+        <asp:Label ID="LabelAttempts" runat="server" Text="Attempts"></asp:Label>
+        <asp:Label ID="LabelAttemptsCount" runat="server" Text="0"></asp:Label>
+        <asp:Label ID="LabelNumberIs" runat="server" Text="The number is "></asp:Label>
+        <asp:Label ID="LabelNum" runat="server" Text="-"></asp:Label>
+    </div>
+
+
+</asp:Content>
---
+++ RestWebApplication/Default.aspx.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.ServiceModel;
+using System.ServiceModel.Activation;
+using System.ServiceModel.Web;
+using System.Net.Http;
+using System.Collections.Specialized;
+using System.Net;
+using System.IO;
+using System.Runtime.Serialization;
+
+namespace RestWebApplication
+{
+    public partial class _Default : Page
+    {
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void ButtonSecretNumber_Click(object sender, EventArgs e)
+        {
+            Uri baseUri = new Uri("http://localhost:60001/Service.svc/");
+            UriTemplate secretNumberTemplate = new UriTemplate("SecretNumber?lower={lower}&upper={upper}");
+            NameValueCollection parameters = new NameValueCollection();
+            parameters.Add("lower", TextBoxLower.Text);
+            parameters.Add("upper", TextBoxUpper.Text);
+
+            Uri completeSecretUri = secretNumberTemplate.BindByName(baseUri, parameters);
+            WebClient proxy = new WebClient(); // create proxy
+            LabelSecret.Text = proxy.DownloadString(completeSecretUri);
+        }
+
+        protected void ButtonPlay_Click(object sender, EventArgs e)
+        {
+            Uri baseUri = new Uri("http://localhost:60001/Service.svc");
+            UriTemplate checkNumberTemplate = new UriTemplate("CheckNumber?userNum={userNum}&secretNum={secretNum}");
+            int attempts = Convert.ToInt32(LabelAttemptsCount.Text);
+            attempts += 1;
+            LabelAttemptsCount.Text = Convert.ToString(attempts);
+
+            NameValueCollection parameters = new NameValueCollection();
+            parameters.Add("userNum", TextBoxMakeGuess.Text);
+            parameters.Add("secretNum", LabelSecret.Text);
+
+            Uri completeCheckUri = checkNumberTemplate.BindByName(baseUri, parameters);
+            WebClient proxy = new WebClient(); // create proxy
+            LabelNum.Text = proxy.DownloadString(completeCheckUri);
+        }
+    }
+}
